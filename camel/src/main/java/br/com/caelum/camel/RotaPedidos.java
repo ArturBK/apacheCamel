94252d852de3a7be08d4875ba1d21a2c77019b04
= <=>
2016-01-05 18:33:06 -0200

    Rotas, Multicast e SEDA.

--- camel/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -10,32 +10,39 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
-		
+
 		context.addRoutes(new RouteBuilder() {
-			
+
 			@Override
 			public void configure() throws Exception {
 
 				from("file:pedidos?delay=5s&noop=true").
-			    setProperty("pedidoId", xpath("/pedido/id/text()")).
-			    setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
-			    split().
-			        xpath("/pedido/itens/item").
-			    filter().
-			        xpath("/item/formato[text()='EBOOK']").
-			    setProperty("ebookId", xpath("/item/livro/codigo/text()")).
-			    log("${id} \n ${body}").
-			    marshal().
-			        xmljson().
-			    setHeader(Exchange.HTTP_QUERY, 
-			            simple("clienteId=${property.clienteId}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
-			    to("http4://localhost:8080/ebook/item");
-				
+				routeId("rota-pedidos").
+				multicast().
+					to("direct:soap").
+					to("direct:http");
+
+				from("direct:soap").
+				routeId("rota-soap").
+				log("chamando servico soap ${body}").
+				to("mock:soap");
+
+				from("direct:http").
+				routeId("rota-http").
+				setProperty("pedidoId", xpath("/pedido/id/text()"))
+				.setProperty("email", xpath("/pedido/pagamento/email-titular/text()")).
+				split().xpath("/pedido/itens/item").
+				filter().xpath("/item/formato[text()='EBOOK']").
+				setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+				setHeader(Exchange.HTTP_QUERY,
+						simple("clienteId=${property.email}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
+				to("http4://localhost:8080/webservices/ebook/item");
+
 			}
 		});
-		
+
 		context.start();
 		Thread.sleep(20000);
 		context.stop();
-	}	
+	}
 }
