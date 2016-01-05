769489c6dc9ee4434eb2ff564c5649e19974de29
= <=>
2016-01-05 18:16:07 -0200

    xpath/setProperty/setHeader/xmlToJason/acessandoWebService

--- camel/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
@@ -16,16 +17,19 @@ public class RotaPedidos {
 			public void configure() throws Exception {
 
 				from("file:pedidos?delay=5s&noop=true").
-				split().
-					xpath("/pedido/itens/item").
-					log("${id}").
-				filter()
-					.xpath("/item/formato[text()='EBOOK']").
-				marshal()
-					.xmljson().
-				log("${body}").
-				setHeader("CamelFileName", simple("${file:name.noext}.json")).
-				to("file:saida");
+			    setProperty("pedidoId", xpath("/pedido/id/text()")).
+			    setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
+			    split().
+			        xpath("/pedido/itens/item").
+			    filter().
+			        xpath("/item/formato[text()='EBOOK']").
+			    setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+			    log("${id} \n ${body}").
+			    marshal().
+			        xmljson().
+			    setHeader(Exchange.HTTP_QUERY, 
+			            simple("clienteId=${property.clienteId}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
+			    to("http4://localhost:8080/ebook/item");
 				
 			}
 		});
